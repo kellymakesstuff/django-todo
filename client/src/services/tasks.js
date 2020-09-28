8ab10e73d7b8ebb6f0ea5f5b1b9f051c75bc35f8
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-09-28 00:11:07 -0400

    apiSetup crud functions

---
+++ client/src/services/tasks.js
@@ -0,0 +1,42 @@
+import api from "./apiSetup"
+
+export const getTaskList = async () => {
+  try {
+    const response = await api.get("/task-list")
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+export const getTaskDetail = async (id) => {
+  try {
+    const response = await api.get(`/task-detail/${id}`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+export const createTask = async (taskData) => {
+  try {
+    const response = await api.post('/task-create', taskData)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+export const updateTask = async (taskData, id) => {
+  try {
+    const response = await api.post(`/task-update/${id}`, taskData)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+export const deleteTask = async (id) => {
+  try {
+    const response = await api.delete(`/task-delete/${id}`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
